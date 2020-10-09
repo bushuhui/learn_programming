3ac67c71ce6804e4323ab7190af82879e1d35fbd
bushuhui <bushuhui@foxmail.com>
2020-10-09 08:41:46 +0800

    Add comments

--- 1_c++/1_basicusage/test.cpp
+++ 1_c++/1_basicusage/test.cpp
@@ -1,7 +1,10 @@
-    #include <iostream>
-    using namespace std;
-    int main()
-    {
-        cout<<"Hello, VScode" <<endl;
-        return 0;
-    }   
+#include <iostream>
+    
+using namespace std;
+
+int main()
+{
+    cout << "Hello, VScode" << endl;
+    return 0;
+}
+
