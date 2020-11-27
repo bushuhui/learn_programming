b1c081c850a7c2d19b24cd93e6651eadfd62e5d1
Xueshaocheng <1209137430@qq.com>
2020-11-28 02:20:18 +0800

    11/27 progress

---
+++ 2_datastructure_algorithm/0_string/Chapter_1_intro.cpp
@@ -0,0 +1,32 @@
+//11/27
+// 算法与数据结构，第一章，入门
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+//malloc 与 free
+
+void mal_free()
+{
+    int *p;
+    p = (int *)malloc(sizeof(int));
+    if (p==NULL)
+        {
+            cerr << "Memory allocation error." << endl;
+            exit(1);
+        }
+    *p = 5;
+    cout << *p;
+    free(p);    //释放p的内存
+    p = NULL;   //p指针赋为空
+}
+
+int main()
+{
+    mal_free();
+
+    return 0;
+}
---
+++ 3_code_practice/array_1.cpp
@@ -0,0 +1,51 @@
+//11/27 练习题
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+//1.删除重复项
+
+template <class T>  //数组长度函数模板
+int length(T &arr)
+{
+    return (sizeof(arr) / sizeof(arr[0]));
+}
+
+int del_repeated(int *arr, int len) //排序数组的处理
+{
+    int i = 0;
+    for (int j = 1; j < len; j++)
+    {
+        if (arr[i] != arr[j])
+        {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return i+1;
+}
+
+void q1()
+{
+    int arr1[5] = {1, 1, 2, 4, 5};
+    int len_new;
+    len_new = del_repeated(arr1, length(arr1));
+    cout << len_new << endl;
+    for (int i = 0; i < len_new; i++)
+    {
+        cout << arr1[i] << ",";
+    }
+}
+
+//2.
+
+int main()
+{
+    q1();
+
+    return 0;
+}
