bdd1f7c5416efe2534692aaf0f863338c20b5c66
Rax_Xue <1209137430@qq.com>
2021-02-21 22:17:34 +0800

    2/21 graph

--- 2_datastructure_algorithm/4_graph/Chapter_8_graph.cpp
+++ 2_datastructure_algorithm/4_graph/Chapter_8_graph.cpp
@@ -1,7 +1,10 @@
-//1/27
+//1/27,2/21
 //数据结构 第八章 图
 
 #include<iostream>
+#include<malloc.h>
+
+#define maxSize 1000
   
 using namespace std;
 
@@ -32,9 +35,121 @@ void adjacency_matrix()
     }
 }
 
+//接邻表：通过链表将图的连接关系表示的方法
+
+typedef struct ArcNode
+{
+    int adjvex;
+    struct ArcNode *nextarc;
+} ArcNode;
+  
+typedef struct
+{
+    int data;
+    ArcNode *firstarc;
+} Vnode;
+  
+//可以利用结构体整体结构，也可以拆分结构体变为单独搜索
+typedef struct
+{
+    Vnode adjlist[maxSize];
+    int n, e;
+} AGraph;
+  
+AGraph *graph;
+  
+//插入链表末尾
+void  insertNode(ArcNode *node, ArcNode *newNode)
+{
+    ArcNode *p = node;
+    while(p->nextarc != NULL) {
+        p = p->nextarc;
+    }
+    p->nextarc = newNode;
+}
+  
+void create()
+{
+    graph = (AGraph*)malloc(sizeof(AGraph));
+    cout << "输入顶点的数目: " << endl;
+    cin >> graph->n;
+    cout << "输入图中边的数目: " << endl;
+    cin >> graph->e;
+  
+    int u = -1, v = -1;
+    for(int i = 0; i < graph->n; i++)
+    {
+        graph->adjlist[i].firstarc = NULL;
+    }
+  
+    ArcNode *node;
+    for(int i = 0; i < graph->e; i++)
+    {
+        cout<<"请输入联通点A与B"<<endl;
+        cin >> u >> v ;
+        node = (ArcNode *)malloc(sizeof(ArcNode));
+        node->adjvex = v;
+        node->nextarc = NULL;
+        graph->adjlist[u].data = u;
+        if(graph->adjlist[u].firstarc == NULL)
+        {
+            //边
+            graph->adjlist[u].firstarc = node;
+        }
+        else
+        {
+            //插入边
+            insertNode(graph->adjlist[u].firstarc, node);
+        }
+    }
+}
+  
+void  travseTree()
+{
+    for(int i = 0; i < graph->n; i++)
+    {
+        if(graph->adjlist[i].firstarc != NULL)
+        {
+            cout <<"与"<< i << "连接的点有：";
+            ArcNode *p = graph->adjlist[i].firstarc;
+            while(p != NULL)
+            {
+                cout << p->adjvex <<  " ";
+                p = p->nextarc;
+            }
+            cout << endl;
+        }
+    }
+}
+
+//链式向前星：将边集数组中每条边按起点从小到大排序，
+//如果起点相同就按照终点从小到大排序，并记录下以某个点为起点的所有边在数组中的起始位置和存储长度
+
+//构造
+struct Edge
+{//表示边
+    int w;
+int to;
+    int next;
+}edge[10005];
+  
+int cnt=0;      //用以控制并统计边的数量
+  
+int head[10005];    //表示来源的边序号
+
+//添加
+void add_edge(int from, int to, int w)
+{
+    edge[cnt].to = to;
+    edge[cnt].w = w;
+    edge[cnt].next = head[from];
+    head[from] = cnt++;
+}
 int main()
 {
-    adjacency_matrix();
+    //adjacency_matrix();
+    create();
+    travseTree();
 
     return 0;
 }
